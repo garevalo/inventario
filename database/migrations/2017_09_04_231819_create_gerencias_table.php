712e5472e312dffa97deb4a4d8b1868374150c51
vagrant <vagrant@homestead>
2017-09-04 23:34:09 +0000

    inventario

---
+++ database/migrations/2017_09_04_231819_create_gerencias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGerenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gerencias', function (Blueprint $table) {
+            $table->increments('idgerencia');
+            $table->string('gerencia',50);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gerencias');
+    }
+}
