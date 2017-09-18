e5fbb312be7078c30f22d850a5ee82630b65df63
Giordan <giordan.arevalo@hotmail.com>
2017-09-18 04:55:04 +0000

    avance

--- database/seeds/CargosTableSeeder.php
+++ database/seeds/CargosTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CargosTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+
+        DB::table('cargos')->insert([
+            'cargo' => str_random(10)
+        ]);
     }
 }
